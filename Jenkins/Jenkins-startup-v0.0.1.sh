a684db1ad9c311162eaf52c7989ca543d4299a05
SHREYAS C <99862109+shrsyc@users.noreply.github.com>
2025-02-15 16:37:37 +0530

    Create Jenkins-startup-v0.0.1.sh
    add first jenkins startup file for aws linux ec2

---
+++ Jenkins/Jenkins-startup-v0.0.1.sh
@@ -0,0 +1,12 @@
+yum update -y
+yum install java-17* -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-17-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
+systemctl start jenkins
+cp /var/lib/jenkins/secrets/initialAdminPassword ./jenkins-password
